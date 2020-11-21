65355ab9062eeb15d50ddaf6bb58c99575851b73
zeyadShapan <zeyadshapan2004@gmail.com>
2020-11-21 18:25:18 +0200

    Added microphone beta

--- app/components/chat/SendMessage.js
+++ app/components/chat/SendMessage.js
@@ -1,24 +1,52 @@
 import React from 'react';
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
+
 import colors from '../../config/colors';
 import Icon from '../Icon';
-
 import TextInput from '../inputs/TextInput';
+import useRecord from "../../hooks/useRecord";
+import logger from "../../utility/logger";
 
 export default function SendMessage({ handleSubmit, text, setText, placeholder }) {
+    const { isRecording, handleRecord } = useRecord(handleUpload);
+
+    async function handleUpload({ uri }) {
+        // const sound = new Audio.Sound();
+        // try {
+        //     await sound.loadAsync(require('../../assets/notif.wav'));
+        //     await sound.playAsync();
+        //     alert("SOUND IS CURRENTLY PLAYING");
+        //     return await sound.unloadAsync();
+        // } catch (error) {
+        //     logger.log(error);
+        // }
+    };
+
     return (
         <View style={styles.container}>
             <TextInput placeholder={placeholder}
                 style={styles.textInput}
                 multiline={true}
                 numberOfLines={4}
-                width="80%"
+                width={text.length > 0 ? "80%" : "60%"}
                 onChangeText={text => setText(text)}
                 value={text}
             />
-            <TouchableOpacity onPress={handleSubmit}>
-                <Icon name="send" size={40} color="white" style={styles.sendMessageContainer} backgroundColor={colors.primary} />
-            </TouchableOpacity>
+            <View style={styles.sendContainer}>
+
+                {text.length > 0 ?
+                    <TouchableOpacity onPress={handleSubmit}>
+                        <Icon name="send" size={40} color="white" backgroundColor={colors.primary} />
+                    </TouchableOpacity>
+                    :
+                    <View style={styles.attatchContainer}>
+                        <MaterialCommunityIcons name="attachment" size={30} onPress={() => { alert("File uploading will be aviable in the next update, stay tuned"); }} />
+                        <MaterialCommunityIcons name="microphone" size={30} color="black" onPress={handleRecord} style={isRecording && styles.activeRecorder} />
+                    </View>
+                }
+            </View>
         </View>
     );
 };
@@ -39,7 +67,15 @@ const styles = StyleSheet.create({
         right: 0,
         bottom: "100%",
     },
-    sendMessageContainer: {
+    sendContainer: {
         top: 4,
+    },
+    attatchContainer: {
+        flexDirection: "row",
+    },
+    activeRecorder: {
+        color: colors.primary,
+        fontSize: 45,
+        marginLeft: 10,
     },
 });
---
+++ app/hooks/useRecord.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Audio } from 'expo-av';
+
+import logger from '../utility/logger';
+
+const useRecord = (handleUpload, uploadParams) => {
+    const [isRecording, setIsRecording] = useState(false);
+    const [recording, setRecording] = useState(new Audio.Recording());
+
+    const handleRecord = async () => {
+        alert("The microphone will be aviable in the next update");
+        const { granted } = await Audio.requestPermissionsAsync();
+        if (!granted) return;
+
+        if (isRecording) {
+            await recording.stopAndUnloadAsync();
+            handleUpload({ uri: recording.getURI(), ...uploadParams });
+            setRecording(new Audio.Recording());
+            return setIsRecording(false);
+        }
+
+        try {
+            await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY);
+            await recording.startAsync();
+            return setIsRecording(true);
+        } catch (error) {
+            alert("Error Occured");
+            logger.log(error);
+        }
+    };
+    return { isRecording, handleRecord };
+};
+
+export default useRecord;
